f738c5485773701be82afee621a9c1eebdff1c45
TopDeveloper <top.developer0221@gmail.com>
2023-02-20 14:20:23 +0100

    webp

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,2 @@
-const DEV_MODE = true;
-export const API_BASE = DEV_MODE == true?'http://localhost:5000/api/v1':'';
-export const API_URL = DEV_MODE == true ?'http://localhost:5000/':'';
+const DEV_MODE = false;
+export const API_BASE = DEV_MODE == true?'http://localhost:5000/api/v1':'http://34.239.248.252:5000/api/v1';
